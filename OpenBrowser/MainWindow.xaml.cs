6b9888ff4c9e6de6ea0dc7c3736fdea37eb3e6b5
Daniel Fros <daniel.fros@grupomaccio.com>
2025-02-24 15:14:29 -0300

    adding all the files

---
+++ OpenBrowser/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows;
+
+namespace OpenBrowser {
+
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window {
+
+		public MainWindow() {
+			InitializeComponent();
+		}
+
+		private void FormMainWindow_Loaded(object sender, RoutedEventArgs e) {
+			webView.Source = new Uri("https://www.google.com");
+		}
+	}
+}
