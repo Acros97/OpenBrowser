78a185387aa56f97e44d4dcfa5697017f49c64f1
Daniel Fros <daniel.fros@grupomaccio.com>
2025-02-24 17:00:49 -0300

    #0.1.0v: Adding search panel and interaction

--- OpenBrowser/MainWindow.xaml.cs
+++ OpenBrowser/MainWindow.xaml.cs
@@ -7,13 +7,65 @@ namespace OpenBrowser {
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+		private const string _HOMEPAGE = "https://duckduckgo.com";          // Can be edited
+
+		#region Form
 
 		public MainWindow() {
 			InitializeComponent();
 		}
 
 		private void FormMainWindow_Loaded(object sender, RoutedEventArgs e) {
-			webView.Source = new Uri("https://www.google.com");
+			webView.Source = new Uri(_HOMEPAGE);
+			txtSearch.Text = _HOMEPAGE;
 		}
+
+		#endregion Form
+
+		#region Buttons
+
+		private void BtnClose_Click(object sender, RoutedEventArgs e) => Close();
+
+		private void BtnMaximize_Click(object sender, RoutedEventArgs e) {
+			switch (WindowState) {
+				case WindowState.Normal:
+					WindowState = WindowState.Maximized;
+					break;
+
+				case WindowState.Maximized:
+					WindowState = WindowState.Normal;
+					break;
+			}
+		}
+
+		private void BtnMinimize_Click(object sender, RoutedEventArgs e) {
+			WindowState = WindowState.Minimized;
+		}
+
+		#endregion Buttons
+
+		#region Events
+
+		private void TxtSearch_KeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
+			if (e.Key == System.Windows.Input.Key.Enter) {
+				try {
+					string newUrl = txtSearch.Text.Trim();
+
+					if (newUrl.StartsWith("www") && !newUrl.StartsWith("http://") && !newUrl.StartsWith("https://"))
+						newUrl = "https://" + newUrl;
+					if (!newUrl.StartsWith("www") && !newUrl.StartsWith("http://") && !newUrl.StartsWith("https://"))
+						newUrl = $"https://duckduckgo.com/?t=h_&q={newUrl}&ia=web";
+
+					webView.Source = new Uri(newUrl);
+					webView.Focus();
+				} catch { }
+			}
+		}
+
+		private void TxtSearch_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e) {
+			txtSearch.Focus();
+		}
+
+		#endregion Events
 	}
 }
